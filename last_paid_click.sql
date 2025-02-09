79fc367d9f018b555b1527180698653a7b6e8ba2
Azirsan <old_town@inbox.ru>
2025-02-09 18:48:51 +0200

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,31 +1,29 @@
-with pain as (
-    select
+WITH pain AS (
+    SELECT
         l.visitor_id,
         s.visit_date,
-        s.source as utm_source,
-        s.medium as utm_medium,
-        s.campaign as utm_campaign,
-        lead_id,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id, 
         l.created_at,
         l.amount,
-        closing_reason,
+        l.closing_reason, 
         l.status_id,
-        ROW_NUMBER()
-            over (
-                partition by l.visitor_id
-                order by l.created_at desc, s.visit_date desc
-            )
-        as row_num
-    from
-        leads as l
-    inner join
-        sessions as s on l.visitor_id = s.visitor_id
-    where
+        ROW_NUMBER() OVER (
+            PARTITION BY l.visitor_id
+            ORDER BY l.created_at DESC, s.visit_date DESC
+        ) AS row_num
+    FROM
+        leads AS l
+    INNER JOIN
+        sessions AS s ON l.visitor_id = s.visitor_id
+    WHERE
         s.visit_date <= l.created_at
-        and medium not in ('organic')
+        AND s.medium NOT IN ('organic')
 )
 
-select
+SELECT
     pain.visitor_id,
     pain.visit_date,
     pain.utm_source,
@@ -36,12 +34,14 @@ select
     pain.amount,
     pain.closing_reason,
     pain.status_id
-from pain
-where pain.row_num = 1
-order by
-    pain.amount desc nulls last,
-    pain.visit_date asc,
-    pain.utm_source asc,
-    pain.utm_medium asc,
-    pain.utm_campaign asc
-limit 10;
+FROM
+    pain
+WHERE
+    pain.row_num = 1
+ORDER BY
+    pain.amount DESC NULLS LAST,
+    pain.visit_date ASC,
+    pain.utm_source ASC,
+    pain.utm_medium ASC,
+    pain.utm_campaign ASC
+LIMIT 10;
