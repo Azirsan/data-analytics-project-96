d8aa73b1f7ea15435a92fd63219aa2bfda99407a
Azirsan <old_town@inbox.ru>
2025-02-17 15:21:54 +0200

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -4,10 +4,10 @@ with pain as (
         l.visitor_id,
         s.visit_date,
         l.created_at,
-        EXTRACT(
+        extract(
             epoch from (l.created_at - s.visit_date) / 86400.0
         ) as time_clouse,
-        ROW_NUMBER() over (
+        row_number() over (
             partition by l.visitor_id
             order by s.visit_date asc
         ) as row_num
@@ -25,174 +25,177 @@ with pain as (
 decil_pain as (
     select
         time_clouse,
-        NTILE(10) over (
+        ntile(10) over (
             order by time_clouse asc
         ) as nt
     from pain
     where row_num = 1
 )
 
-select ROUND(MAX(time_clouse)::numeric, 0) as fianl_day
+select round(max(time_clouse)::numeric, 0) as final_day
 from decil_pain
 where nt <= 9;
+
 -- МЕТРИКИ
-WITH pain AS (
-    SELECT
+with pain as (
+    select
         l.visitor_id,
-        s.source AS utm_source,
-        s.medium AS utm_medium,
-        s.campaign AS utm_campaign,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
         l.lead_id,
-        l.created_at,
+        l.created_at, 
         l.amount,
         l.closing_reason,
         l.status_id,
-        DATE(s.visit_date) AS visit_date,
-        ROW_NUMBER() OVER (
-            PARTITION BY s.visitor_id
-            ORDER BY s.visit_date DESC
-        ) AS row_num
-    FROM
-        sessions AS s
-    LEFT JOIN
-        leads AS l
-        ON
+        date(s.visit_date) as visit_date,
+        row_number() over (
+            partition by s.visitor_id
+            order by s.visit_date desc
+        ) as row_num
+    from
+        sessions as s
+    left join
+        leads as l
+        on
             s.visitor_id = l.visitor_id
-            AND s.visit_date <= l.created_at
-    WHERE s.medium != 'organic'
+            and s.visit_date <= l.created_at
+    where s.medium != 'organic'
 ),
 
-ads_total AS (
-    SELECT
-        DATE(campaign_date) AS camp_date,
+ads_total as (
+    select
+        date(campaign_date) as camp_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        SUM(
-            COALESCE(daily_spent, 0)
-        ) AS total_cost
-    FROM
+        sum(
+            coalesce(daily_spent, 0)
+        ) as total_cost
+    from
         ya_ads
-    GROUP BY
-        DATE(campaign_date),
+    group by
+        date(campaign_date),
         utm_source,
         utm_medium,
         utm_campaign
-    UNION ALL
-    SELECT
-        DATE(campaign_date) AS camp_date,
+    union all
+    select
+        date(campaign_date) as camp_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        SUM(
-            COALESCE(daily_spent, 0)
-        ) AS total_cost
-    FROM
+        sum(
+            coalesce(daily_spent, 0)
+        ) as total_cost
+    from
         vk_ads
-    GROUP BY
-        DATE(campaign_date),
+    group by
+        date(campaign_date),
         utm_source,
         utm_medium,
         utm_campaign
 ),
 
-fianl AS (
-    SELECT
+final as (
+    select
         pain.visit_date,
         pain.utm_source,
         pain.utm_medium,
         pain.utm_campaign,
         adt.total_cost,
-        COUNT(*) AS visitors_count,
-        COUNT(pain.lead_id) AS leads_count,
-        SUM(CASE
-            WHEN pain.status_id = 142 THEN 1
-            ELSE 0
-        END) AS purchases_count,
-        SUM(CASE
-            WHEN pain.status_id = 142 THEN pain.amount
-            ELSE 0
-        END) AS revenue
-    FROM
+        count(*) as visitors_count,
+        count(pain.lead_id) as leads_count,
+        sum(case
+            when pain.status_id = 142 then 1
+            else 0
+        end) as purchases_count,
+        sum(case
+            when pain.status_id = 142 then pain.amount
+            else 0
+        end) as revenue
+    from
         pain
-    LEFT JOIN ads_total AS adt
-        ON
+    left join ads_total as adt
+        on
             pain.visit_date = adt.camp_date
-            AND pain.utm_source = adt.utm_source
-            AND pain.utm_medium = adt.utm_medium
-            AND pain.utm_campaign = adt.utm_campaign
-    WHERE
+            and pain.utm_source = adt.utm_source
+            and pain.utm_medium = adt.utm_medium
+            and pain.utm_campaign = adt.utm_campaign
+    where
         pain.row_num = 1
-    GROUP BY
+    group by
         pain.utm_source,
         pain.utm_medium,
         pain.utm_campaign,
-        DATE(pain.visit_date),
+        date(pain.visit_date),
         adt.total_cost
 ) -- можно расчет тут сделать, но код становится нечитаемым
 
-SELECT
-    /*  visit_date,*/
+select
+    -- visit_date,
     utm_source,
-    /*utm_medium,
-    utm_campaign,*/
-    ROUND(SUM(total_cost) / SUM(visitors_count), 2) AS cpu,
-    ROUND(SUM(total_cost) / SUM(leads_count), 2) AS cpl,
-    ROUND(SUM(total_cost) / SUM(purchases_count), 2) AS cppu,
-    ROUND(100 * (SUM(revenue) - SUM(total_cost)) / SUM(total_cost), 2) AS roi,
-    ROUND(100 * SUM(leads_count) / SUM(visitors_count), 2) AS lcr,
-    ROUND(100 * SUM(purchases_count) / SUM(leads_count), 2) AS lscr
-FROM fianl
-GROUP BY utm_source
-HAVING ROUND(SUM(total_cost) / SUM(visitors_count), 2) IS NOT null
-ORDER BY utm_source DESC;
+    -- utm_medium,
+    -- utm_campaign,
+    round(sum(total_cost) / sum(visitors_count), 2) as cpu,
+    round(sum(total_cost) / sum(leads_count), 2) as cpl,
+    round(sum(total_cost) / sum(purchases_count), 2) as cppu,
+    round(100 * (sum(revenue) - sum(total_cost)) / sum(total_cost), 2) as roi,
+    round(100 * sum(leads_count) / sum(visitors_count), 2) as lcr,
+    round(100 * sum(purchases_count) / sum(leads_count), 2) as lscr
+from final
+group by utm_source
+having round(sum(total_cost) / sum(visitors_count), 2) is not null
+order by utm_source desc;
+
 -- анализ посещаемости по неделям
-SELECT
-    medium AS utm_medium,
-    campaign AS utm_campaign,
-    CASE
-        WHEN LOWER(source) LIKE '%admitad%' THEN 'admitad'
-        WHEN LOWER(source) LIKE '%baidu%' THEN 'baidu.com'
-        WHEN LOWER(source) LIKE '%bing%' THEN 'bing.com'
-        WHEN LOWER(source) LIKE '%botmother%' THEN 'botmother'
-        WHEN LOWER(source) LIKE '%dzen%' THEN 'dzen'
-        WHEN LOWER(source) LIKE '%facebook%' THEN 'facebook.com'
-        WHEN LOWER(source) LIKE '%go.mail.ru%' THEN 'go.mail.ru'
-        WHEN LOWER(source) LIKE '%google%' THEN 'google'
-        WHEN LOWER(source) LIKE '%instagram%' THEN 'instagram'
-        WHEN LOWER(source) LIKE '%mytarget%' THEN 'mytarget'
-        WHEN LOWER(source) LIKE '%organic%' THEN 'organic'
-        WHEN LOWER(source) LIKE '%partners%' THEN 'partners'
-        WHEN LOWER(source) LIKE '%podcast%' THEN 'podcast'
-        WHEN LOWER(source) LIKE '%public%' THEN 'public'
-        WHEN LOWER(source) LIKE '%rutube%' THEN 'rutube'
-        WHEN LOWER(source) LIKE '%search.ukr.net%' THEN 'search.ukr.net'
-        WHEN LOWER(source) LIKE '%slack%' THEN 'slack'
-        WHEN LOWER(source) LIKE '%social%' THEN 'social'
-        WHEN
-            LOWER(source) LIKE '%telegram%'
-            OR LOWER(source) = 'telegram.me'
-            OR LOWER(source) = 'tg'
-            THEN 'telegram'
-        WHEN LOWER(source) LIKE '%timepad%' THEN 'timepad'
-        WHEN LOWER(source) LIKE '%tproger%' THEN 'tproger'
-        WHEN LOWER(source) LIKE '%twitter%' THEN 'twitter.com'
-        WHEN LOWER(source) LIKE '%vc%' THEN 'vc'
-        WHEN
-            LOWER(source) LIKE '%vk%'
-            OR LOWER(source) IN ('vk.com', 'vk-group', 'vkontakte', 'vk-senler')
-            THEN 'vk'
-        WHEN
-            LOWER(source) LIKE '%yandex%'
-            OR LOWER(source) IN ('yandex.com', 'yandex-direct')
-            THEN 'yandex'
-        WHEN
-            LOWER(source) LIKE '%zen%' OR LOWER(source) IN ('zen_from_telegram')
-            THEN 'dzen'
-        ELSE source
-    END AS utm_source,
-    EXTRACT(WEEK FROM visit_date) - 21 AS week_number,
-    COUNT(*) AS visits
-FROM
+select
+    medium as utm_medium,
+    campaign as utm_campaign,
+    case
+        when lower(source) like '%admitad%' then 'admitad'
+        when lower(source) like '%baidu%' then 'baidu.com'
+        when lower(source) like '%bing%' then 'bing.com'
+        when lower(source) like '%botmother%' then 'botmother'
+        when lower(source) like '%dzen%' then 'dzen'
+        when lower(source) like '%facebook%' then 'facebook.com'
+        when lower(source) like '%go.mail.ru%' then 'go.mail.ru'
+        when lower(source) like '%google%' then 'google'
+        when lower(source) like '%instagram%' then 'instagram'
+        when lower(source) like '%mytarget%' then 'mytarget'
+        when lower(source) like '%organic%' then 'organic'
+        when lower(source) like '%partners%' then 'partners'
+        when lower(source) like '%podcast%' then 'podcast'
+        when lower(source) like '%public%' then 'public'
+        when lower(source) like '%rutube%' then 'rutube'
+        when lower(source) like '%search.ukr.net%' then 'search.ukr.net'
+        when lower(source) like '%slack%' then 'slack'
+        when lower(source) like '%social%' then 'social'
+        when
+            lower(source) like '%telegram%'
+            or lower(source) = 'telegram.me'
+            or lower(source) = 'tg'
+            then 'telegram'
+        when lower(source) like '%timepad%' then 'timepad'
+        when lower(source) like '%tproger%' then 'tproger'
+        when lower(source) like '%twitter%' then 'twitter.com'
+        when lower(source) like '%vc%' then 'vc'
+        when
+            lower(source) like '%vk%'
+            or lower(source) in ('vk.com', 'vk-group', 'vkontakte', 'vk-senler')
+            then 'vk'
+        when
+            lower(source) like '%yandex%'
+            or lower(source) in ('yandex.com', 'yandex-direct')
+            then 'yandex'
+        when
+            lower(source) like '%zen%'
+            or lower(source) in ('zen_from_telegram')
+            then 'dzen'
+        else source
+    end as utm_source,
+    extract(week from visit_date) - 21 as week_number,
+    count(*) as visits
+from
     sessions
-GROUP BY utm_source, medium, campaign, EXTRACT(WEEK FROM visit_date) - 21;
+group by utm_source, medium, campaign, extract(week from visit_date) - 21;
