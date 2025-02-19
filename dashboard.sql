a695efc316ce7049a66f2c29fff78287de0dcb53
Azirsan <old_town@inbox.ru>
2025-02-19 18:05:29 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,177 +1,168 @@
 -- 90 % закрытия лидов
-with pain as (
-    select
+WITH pain AS (
+    SELECT
         l.visitor_id,
         s.visit_date,
         l.created_at,
-        AGE(l.created_at, s.visit_date) as time_clouse,
-        ROW_NUMBER() over (
-            partition by l.visitor_id
-            order by s.visit_date asc
-        ) as row_num
-    from
-        leads as l
-    inner join
-        sessions as s
-        on
-            l.visitor_id = s.visitor_id
-            and l.created_at >= s.visit_date
-            and s.medium != 'organic'
-    where l.status_id = 142
+        AGE(l.created_at, s.visit_date) AS time_clouse,
+        ROW_NUMBER() OVER (
+            PARTITION BY l.visitor_id
+            ORDER BY s.visit_date ASC
+        ) AS row_num
+    FROM
+        leads AS l
+    INNER JOIN
+        sessions AS s
+        ON l.visitor_id = s.visitor_id
+            AND l.created_at >= s.visit_date
+            AND s.medium != 'organic'
+    WHERE l.status_id = 142
 ),
 
-decil_pain as (
-    select
+decil_pain AS (
+    SELECT
         time_clouse,
-        NTILE(10) over (
-            order by time_clouse asc
-        ) as nt
-    from pain
-    where row_num = 1
+        NTILE(10) OVER (
+            ORDER BY time_clouse ASC
+        ) AS nt
+    FROM pain
+    WHERE row_num = 1
 )
 
-select MAX(time_clouse) as final_day
-from decil_pain
-where nt <= 9;
+SELECT MAX(time_clouse) AS final_day
+FROM decil_pain
+WHERE nt <= 9;
 
 -- МЕТРИКИ
-with pain as (
-    select
+WITH pain AS (
+    SELECT
         l.visitor_id,
-        s.source as utm_source,
-        s.medium as utm_medium,
-        s.campaign as utm_campaign,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
         l.status_id,
-        date(s.visit_date) as visit_date,
-        row_number() over (
-            partition by s.visitor_id
-            order by s.visit_date desc
-        ) as row_num
-    from
-        sessions as s
-    left join
-        leads as l
-        on
-            s.visitor_id = l.visitor_id
-            and s.visit_date <= l.created_at
-    where s.medium != 'organic'
+        DATE(s.visit_date) AS visit_date,
+        ROW_NUMBER() OVER (
+            PARTITION BY s.visitor_id
+            ORDER BY s.visit_date DESC
+        ) AS row_num
+    FROM
+        sessions AS s
+    LEFT JOIN
+        leads AS l
+        ON s.visitor_id = l.visitor_id
+            AND s.visit_date <= l.created_at
+    WHERE s.medium != 'organic'
 ),
 
-ads_total as (
-    select
-        date(campaign_date) as camp_date,
+ads_total AS (
+    SELECT
+        DATE(campaign_date) AS camp_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        sum(
-            coalesce(daily_spent, 0)
-        ) as total_cost
-    from
+        SUM(
+            COALESCE(daily_spent, 0)
+        ) AS total_cost
+    FROM
         ya_ads
-    group by
-        date(campaign_date),
+    GROUP BY
+        DATE(campaign_date),
         utm_source,
         utm_medium,
         utm_campaign
-    union all
-    select
-        date(campaign_date) as camp_date,
+    UNION ALL
+    SELECT
+        DATE(campaign_date) AS camp_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        sum(
-            coalesce(daily_spent, 0)
-        ) as total_cost
-    from
+        SUM(
+            COALESCE(daily_spent, 0)
+        ) AS total_cost
+    FROM
         vk_ads
-    group by
-        date(campaign_date),
+    GROUP BY
+        DATE(campaign_date),
         utm_source,
         utm_medium,
         utm_campaign
 ),
 
-final as (
-    select
+final AS (
+    SELECT
         pain.visit_date,
         pain.utm_source,
         pain.utm_medium,
         pain.utm_campaign,
         adt.total_cost,
-        count(*) as visitors_count,
-        count(pain.lead_id) as leads_count,
-        sum(case
-            when pain.status_id = 142 then 1
-            else 0
-        end) as purchases_count,
-        sum(case
-            when pain.status_id = 142 then pain.amount
-            else 0
-        end) as revenue
-    from
+        COUNT(*) AS visitors_count,
+        COUNT(pain.lead_id) AS leads_count,
+        SUM(CASE
+            WHEN pain.status_id = 142 THEN 1
+            ELSE 0
+        END) AS purchases_count,
+        SUM(CASE
+            WHEN pain.status_id = 142 THEN pain.amount
+            ELSE 0
+        END) AS revenue
+    FROM
         pain
-    left join ads_total as adt
-        on
-            pain.visit_date = adt.camp_date
-            and pain.utm_source = adt.utm_source
-            and pain.utm_medium = adt.utm_medium
-            and pain.utm_campaign = adt.utm_campaign
-    where
+    LEFT JOIN ads_total AS adt
+        ON pain.visit_date = adt.camp_date
+            AND pain.utm_source = adt.utm_source
+            AND pain.utm_medium = adt.utm_medium
+            AND pain.utm_campaign = adt.utm_campaign
+    WHERE
         pain.row_num = 1
-    group by
+    GROUP BY
         pain.utm_source,
         pain.utm_medium,
         pain.utm_campaign,
-        date(pain.visit_date),
+        DATE(pain.visit_date),
         adt.total_cost
-) -- можно расчет тут сделать, но код становится нечитаемым
+) 
 
-select
+SELECT
     -- visit_date,
     utm_source,
     -- utm_medium,
     -- utm_campaign,
-    round(sum(total_cost) / sum(visitors_count), 2) as cpu,
-    round(sum(total_cost) / sum(leads_count), 2) as cpl,
-    round(sum(total_cost) / sum(purchases_count), 2) as cppu,
-    round(100 * (sum(revenue) - sum(total_cost)) / sum(total_cost), 2) as roi,
-    round(100 * sum(leads_count) / sum(visitors_count), 2) as lcr,
-    round(100 * sum(purchases_count) / sum(leads_count), 2) as lscr
-from final
-group by utm_source
-having round(sum(total_cost) / sum(visitors_count), 2) is not null
-order by utm_source desc;
+    ROUND(SUM(total_cost) / SUM(visitors_count), 2) AS cpu,
+    ROUND(SUM(total_cost) / SUM(leads_count), 2) AS cpl,
+    ROUND(SUM(total_cost) / SUM(purchases_count), 2) AS cppu,
+    ROUND(100 * (SUM(revenue) - SUM(total_cost)) / SUM(total_cost), 2) AS roi,
+    ROUND(100 * SUM(leads_count) / SUM(visitors_count), 2) AS lcr,
+    ROUND(100 * SUM(purchases_count) / SUM(leads_count), 2) AS lscr
+FROM final
+GROUP BY utm_source
+HAVING ROUND(SUM(total_cost) / SUM(visitors_count), 2) IS NOT NULL
+ORDER BY utm_source DESC;
 
 -- анализ посещаемости по неделям
-select
-    medium as utm_medium,
-    campaign as utm_campaign,
-    case
-        when
-            lower(source) like '%telegram%'
-            or lower(source) = 'tg'
-            then 'telegram'
-        when lower(source) like '%vk%'
-            then 'vk'
-        when
-            lower(source) like '%yandex%'
-            then 'yandex'
-        else source
-    end as utm_source,
-    extract(week from visit_date)
-    - extract(week from date_trunc('month', visit_date))
-    + 1 as week_number,
-    count(*) as visits
-from
-    sessions
-group by
+SELECT
+    medium AS utm_medium,
+    campaign AS utm_campaign,
+    CASE
+        WHEN lower(source) LIKE '%telegram%'
+            OR lower(source) = 'tg' THEN 'telegram'
+        WHEN lower(source) LIKE '%vk%' THEN 'vk'
+        WHEN lower(source) LIKE '%yandex%' THEN 'yandex'
+        ELSE source
+    END AS utm_source,
+    EXTRACT(week FROM visit_date)
+    - EXTRACT(week FROM DATE_TRUNC('month', visit_date))
+    + 1 AS week_number,
+    COUNT(*) AS visits
+FROM sessions
+GROUP BY
     utm_source,
     medium,
     campaign,
-    extract(week from visit_date)
-    - extract(week from date_trunc('month', visit_date))
-    + 1
+    EXTRACT(week FROM visit_date)
+    - EXTRACT(week FROM DATE_TRUNC('month', visit_date))
+    + 1;
